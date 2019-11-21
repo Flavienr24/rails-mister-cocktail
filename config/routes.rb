9e99f792c7565ab84c1e491af2cd2ec0b3ab0fdf
Flavien Rosellini <rosellini.flavien@gmail.com>
2019-11-21 18:15:14 +0100

    Mr Cocktail in progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails do
+    resources :doses
+  end
+
+  root to: 'cocktails#index'
 end
+
+# show => cocktails, ingredient
+# index => cocktail
+# new => cocktail, dose
+# create => cocktail, dose
+# edit => cocktail, dose
+# update => cocktail, dose
+# delete => cocktail, dose
